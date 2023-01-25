82b905fb546b519d925ff0c71bb2af3c8dd52653
shidqiadiatma <adiatma944@gmail.com>
2023-01-25 19:47:51 +0700

    first project

---
+++ Test Suites/Mobile-secondhand/Register Secondhand Apps.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Register Secondhand Apps</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>a7a1c511-ae53-416a-b8d8-76da569587f2</testSuiteGuid>
+</TestSuiteEntity>
