46ec37d8518c0b5a793118867f37652c23960e1d
regitafd.0726 <regitafd.0726@gmail.com>
2023-02-04 10:58:40 +0700

    added test suite

---
+++ Test Suites/Web-secondhand/web log out.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>web log out</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>8e50bb76-cb97-41ee-8a97-2656f18cd6aa</testSuiteGuid>
+   <testCaseLink>
+      <guid>202066ae-b065-4a87-8db1-dbbfacef8df3</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Web Secondhand/Test Scenario/TS08-Logout/TC.08.01-Logout</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
