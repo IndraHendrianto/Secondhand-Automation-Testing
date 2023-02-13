b02dfc2880457c8e96e212ad74bbfbcc0c4e117b
shidqiadiatma <adiatma944@gmail.com>
2023-02-13 10:42:12 +0700

    fix mobile  test suites

---
+++ Test Suites/Mobile-secondhand/06. Mobile Buy Product.ts
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>06. Mobile Buy Product</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>661d1858-a516-44d9-b71f-71816fd1df40</testSuiteGuid>
+   <testCaseLink>
+      <guid>b0807b5b-84de-4b51-b33f-15a9106b9220</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Mobile Secondhand/Test Scenario/TS.007-Buy Product/TC.007.001-Negative Buy Product</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>11a14929-96b1-4a6b-8a1c-9e4691533274</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Mobile Secondhand/Test Scenario/TS.007-Buy Product/TC.007.001-Positive Buy Product</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
