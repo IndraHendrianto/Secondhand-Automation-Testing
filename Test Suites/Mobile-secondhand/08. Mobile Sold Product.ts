8f00505a87dfa4f5240dab1e03f1b53c2ddf4612
shidqiadiatma <adiatma944@gmail.com>
2023-02-22 09:37:28 +0700

    added mobile test suites sold product

---
+++ Test Suites/Mobile-secondhand/08. Mobile Sold Product.ts
@@ -0,0 +1,35 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>08. Mobile Sold Product</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>e878ac1f-3963-468b-9339-9bcfc175d512</testSuiteGuid>
+   <testCaseLink>
+      <guid>42f1992e-2200-4a44-a887-75dcd90e53b0</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Mobile Secondhand/Test Scenario/TS.009-Sold Product/TC.009.001-Product Success Sold</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>6d8e5ae5-59e4-4063-946a-907c5dac5d6a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Mobile Secondhand/Test Scenario/TS.009-Sold Product/TC.009.002-Reject Order</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>c3bd2f4b-eee8-44d1-ae04-2fa0943ffa04</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Mobile Secondhand/Test Scenario/TS.009-Sold Product/TC.009.003-Cancel Order</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
